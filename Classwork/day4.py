1746516f00b30c9c359e316ed3ac5806ccbbbfc9
vikashkothagolla <129354077+vikashkothagolla@users.noreply.github.com>
2025-07-01 06:17:15 +0000

    classwork

--- Classwork/day4.py
+++ Classwork/day4.py
@@ -1,4 +1,16 @@
-import keyword
-
-print(keyword.kwlist) 
-print(len(keyword.kwlist))
+myvar=10 ##Valid Assignment
+mYvar=10 ##Valid Assignment
+My_var=10 ##Valid Assignment
+# my@var=10 ##SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+_myvar=10 ##Valid Assignment
+# @myvar=10 ##SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+# my var = 10 ##SyntaxError: invalid syntax
+myvar=10 #case senstive
+Myvar=20 #case senstive
+a = 10 #storing the values into varibles
+b = 20 #Stroing the values into varibles
+# 10 20 #Output before swaping
+a,b = b,a #Swaping operation is being performed
+# 20 10 #Output after swaping the varibles
+a,b,c = 10,20,30
+print("a,b,c") #10,20,30 is the Output
