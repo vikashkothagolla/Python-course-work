b0f1c54e9b36526e5533e5eabb78f6338f84db71
vikashkothagolla <129354077+vikashkothagolla@users.noreply.github.com>
2025-07-02 05:50:19 +0000

    Classwork

---
+++ Classwork/#4,pythonoperators.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+print("addition(+):",a+b)
+print("substraction(-):",a-b)
+print("multiplication(*):",a*b)
+print("division(/):",a/b)
+print("floor divison(//):",a//b)
+print("moduls(%):",a%b)
+print("expoential(**):",a**b)
+
