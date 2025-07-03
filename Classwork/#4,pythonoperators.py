c575a07f9c89a4f331eacaa8e2027f18049619cd
vikashkothagolla <129354077+vikashkothagolla@users.noreply.github.com>
2025-07-03 12:30:41 +0000

    Classwork

--- Classwork/#4,pythonoperators.py
+++ Classwork/#4,pythonoperators.py
@@ -57,3 +57,63 @@ print(a is  b)
 print(b is  a)
 print(c is  a)
 print(c is not  b)
+
+#bitwise operator
+a=2
+b=4
+print("and operator:",a&b)
+print("or operator:",a|b)            
+print("XOR operator:",a^b)
+print("Not operator:",~b)
+print("left shift operator:",a<<b)
+print("right shift operator:",a>>b)
+
+# operations of strings
+n1="vikash"
+n2="yadav"
+print("concatenation of strings:",n1+n2)
+print("vikash" *3)
+print(n1[4])
+print(n2[2])
+print(n1[0:4])
+print(n2[:2])
+print('vikash' in n1)
+print("python" not in n2)
+
+#built in string functions
+k="python programming"
+print(len(k))
+print(max('Vikash'))
+print(min("Vikash"))
+print(sorted('python'))
+print(chr(97))
+print(ord('s'))
+
+#python string methods 
+# case conversion methods
+x="health is wealtth"
+print(x.upper())
+print(x.lower())
+print(x.capitalize())
+print(x.title())
+y='PyThOn'
+print(y.swapcase())
+
+
+# Alignment and formatting methods
+y='tit for tat'
+print(y.center(30,"*"))
+print(y.center(20,"_"))
+print(y.rjust(20,'_'))
+print(y.ljust(20,"_"))
+w="899"
+print(w.zfill(5))
+
+
+#search and find methods
+u="varunsanjaytharun"
+print(u.find("sanjay"))
+print(u.find('s'))
+print(u.count('a'))
+print(u.index('v'))
+print(u.rfind("a"))
