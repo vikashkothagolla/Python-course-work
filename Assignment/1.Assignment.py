202a5f6b2f5043210788493b6da4004798680dd9
vikashkothagolla <129354077+vikashkothagolla@users.noreply.github.com>
2025-07-11 10:35:34 +0000

    Assignment

---
+++ Assignment/1.Assignment.py
@@ -0,0 +1,13 @@
+
+pnr_id = int(input("Enter PNR ID: "))
+passenger_name = input("Enter Passenger Name: ")
+ticket_price = float(input("Enter the Price of Ticket: "))
+booking_type = input("Enter Reservation Booking Type: ")
+available_tickets = int(input("Enter Available Tickets while booking: "))
+sold_tickets = int(input("Enter Sold Tickets while booking: "))
+aged_discount = float(input("Enter Discount for Aged Passengers: "))
+reservation_type = input("Enter Reservation Type: ")
+organisation_name = input("Enter the Organisation Name: ")
+railway_contact = input("Enter the Railway Contact Number: ")
+management_location = input("Enter the Management Location: ")
+
