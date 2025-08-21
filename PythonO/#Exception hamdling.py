c6fdd645b7daad203195f2b251acd38ae3018609
vikashkothagolla <217r1a67f6@cmrtc.ac.in>
2025-08-21 12:14:50 +0530

    PythonO

--- PythonO/#Exception hamdling.py
+++ PythonO/#Exception hamdling.py
@@ -4,12 +4,13 @@
 
 
 # exceptional handling
-try:
+'''try:
     a=10
     a=a+10
     l=[1,2,3]
     k=l[2]
     d={1:2,2:4,3:9}
+    b=int(input("Enter a number: "))
     print(b)
     c=1+"1"
 except NameError:
@@ -20,3 +21,24 @@ except KeyError:
     print("key is not present")
 except TypeError:
     print("no two data types are added")        
+# or instead all 4 types of erros in above,we can write in single line as below
+except(NameError,IndexError,KeyError,ValueError,TypeError) as e:
+    print(f"Error occured: {e}")'''
+
+try:
+    a=10
+    a=a+10
+    l=[1,2,3]
+    k=l[2]
+    d={1:2,2:4,3:9}
+    b=int(input("Enter a number: "))
+    print(b)
+    c=1+12
+except Exception as e:
+    print(f"Error occured: {e}")
+else:
+    print("No errors")
+    print(c)
+finally:
+    print("---------code executed--------------")
+
